c0c41e0fa8edc191d3fcb3ff6eb01447a32236b6
Paulo Rodrigues <paulo.rodrigues@rebase.com.br>
2021-08-13 21:31:54 -0300

    auth service signup on controller

--- src/users/auth.service.ts
+++ src/users/auth.service.ts
@@ -1,5 +1,9 @@
 import { Injectable, BadRequestException } from '@nestjs/common';
+import { randomBytes, scrypt as _scrypt } from 'crypto';
+import { promisify } from 'util';
 import { UsersService } from './users.service';
+
+const scrypt = promisify(_scrypt);
 
 @Injectable()
 export class AuthService {
@@ -11,6 +15,15 @@ export class AuthService {
     if (users.length) {
       throw new BadRequestException('email already in use');
     }
+    const salt = randomBytes(8).toString('hex');
+
+    const hash = (await scrypt(password, salt, 32)) as Buffer;
+
+    const result = salt + '.' + hash.toString('hex');
+
+    const user = await this.usersService.create(email, result);
+
+    return user;
   }
 
   signIn() {
